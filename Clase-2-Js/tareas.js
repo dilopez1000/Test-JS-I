528c9fadc5087659ec17262a7543c42080886495
unknown <dilopez1000@gmail.com>
2023-01-21 18:29:11 -0300

    prueba-cambio(2)-git

--- Clase-2-Js/tareas.js
+++ Clase-2-Js/tareas.js
@@ -12,7 +12,7 @@
 const nuevaString = 'JavaScript';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 77;
 
 // Crea una variable booleana:
 const nuevoBool = null;
